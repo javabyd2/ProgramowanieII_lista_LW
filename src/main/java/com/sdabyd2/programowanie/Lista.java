18230597e791463779e7fdda34ffed37ea790396
Lukasz <lukww@wp.pl>
2018-02-10 15:29:31 +0100

    First commit

---
+++ src/main/java/com/sdabyd2/programowanie/Lista.java
@@ -0,0 +1,17 @@
+package com.sdabyd2.programowanie;
+
+public class Lista {
+    int [] liczby;
+    int pojemnosc;
+    int rozmiar;
+
+    public Lista(int[] liczby, int pojemnosc, int rozmiar) {
+        this.liczby = liczby;
+        this.pojemnosc = pojemnosc;
+        this.rozmiar = rozmiar;
+    }
+    public int dodajElement(){
+
+        return 0;
+    }
+}
